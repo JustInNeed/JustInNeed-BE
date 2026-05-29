420e420fdc54afa4b80a8db9591dd1171312f492
optiprime27 <yesong0427@gmail.com>
2026-05-30 01:28:25 +0900

    chore: spring initializr 파일 추가

---
+++ src/main/java/com/justinneed/justinneed/JustinneedApplication.java
@@ -0,0 +1,13 @@
+package com.justinneed.justinneed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustinneedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustinneedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justinneed/justinneed/JustinneedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justinneed.justinneed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustinneedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
